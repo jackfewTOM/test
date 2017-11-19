999f41a1e194a704054d34c0ff880d278932e439
wangfew <524544515@qq.com>
2017-11-19 17:54:51 +0800

    first commit

---
+++ test/test/src/main/java/StartServer.java
@@ -0,0 +1,14 @@
+
+public class StartServer {
+
+	public static void main(String[] args) {
+
+		System.out.println("Netty4.0 服务开启");
+		System.out.println("Port:53606");
+
+		NettyServer ns = new NettyServer();
+		ns.bind();
+
+	}
+
+}
